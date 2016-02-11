3e2ac89db18b30918e6fc3a2bf025156801f2d0d
Ben Norris <ben@bsn.io>
2016-02-11 15:11:32 -0700

    Initial blank framework

---
+++ CustomTabBar/CustomTabBar.h
@@ -0,0 +1,19 @@
+//
+//  CustomTabBar.h
+//  CustomTabBar
+//
+//  Created by Ben Norris on 2/11/16.
+//  Copyright © 2016 BSN Design. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CustomTabBar.
+FOUNDATION_EXPORT double CustomTabBarVersionNumber;
+
+//! Project version string for CustomTabBar.
+FOUNDATION_EXPORT const unsigned char CustomTabBarVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CustomTabBar/PublicHeader.h>
+
+
--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# CustomTabBar
+# CustomTabBar [![GitHub license](https://img.shields.io/badge/license-MIT-lightgrey.svg)](https://raw.githubusercontent.com/Carthage/Carthage/master/LICENSE.md) [![Carthage compatible](https://img.shields.io/badge/Carthage-compatible-4BC51D.svg?style=flat)](https://github.com/Carthage/Carthage)
 An iOS component for a custom tab bar
